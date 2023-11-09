67bedd65211b3e3a60bc44806fcb48c1a62c64cc
degradka <69397649+degradka@users.noreply.github.com>
2023-11-09 16:37:22 +0300

    Вложения Ве_Крест.h

---
+++ header/Ве_Крест.h
@@ -0,0 +1,81 @@
+#ifndef Байкал
+#define Байкал
+
+#ifdef _WIN32
+#include <Windows.h>
+#define вперёд_славяне SetConsoleOutputCP(1251)
+
+#endif
+
+#include <stdio.h>
+
+#define царь_батюшка_главный int main
+#define внедрить using
+#define молвить printf
+#define внемлить scanf
+#define воздать return
+
+#define пахать do
+#define покуда while
+#define для for
+
+#define коли if
+#define отнюдь else
+
+#define династия struct
+#define семейство struct
+#define перепись enum
+
+#define бить_ящеров break
+#define добить_ящеров continue
+#define путевой_камень switch
+#define путь case
+#define базированно default
+
+#define правда 1
+#define кривда 0
+
+#define прыг_скок '\n'
+#define плод_родовой malloc
+#define казнь free
+#define туда_не_знаю_куда NULL
+#define НИЧТО NULL
+#define судьба rand
+#define приказ_княжий const
+#define мощь pow
+#define получи_басурман throw
+#define гнев_Перуна exception
+#define окстись goto
+#define свет_мой_зеркальце template
+#define скажи typedef
+#define царский private
+#define народный public
+#define дружинный protected
+#define встрой inline
+#define кощей static
+#define однобокий unsigned
+#define пытать_ящера try
+#define поймать_ящера catch
+#define хутор namespace
+#define откупорить fopen
+#define закупорить fclose
+#define суд_Перуна auto
+#define очами_стрельнуть fgetc
+
+внедрить целина = int;
+внедрить вель_дробь = double;
+внедрить малый_дробь = float;
+внедрить буква = char;
+внедрить много_букав = char*;
+внедрить бестолочь = void;
+внедрить карлик = short;
+внедрить долговязый = long;
+внедрить Петр_Первый = long long;
+внедрить мерило = size_t;
+внедрить розсуд = int;
+
+
+перепись счёт_древних_русов{ ноль = 0, целковый = 1, полушка = 2, четвертушка = 3, осьмушка = 4,
+пудовичок = 5, медячок = 6, серебрячок = 7, золотничок = 8, девятичок = 9, десятичок = 10 };
+
+#endif
